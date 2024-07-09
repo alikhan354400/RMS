dbcb4db40b932b337a9508d1b5baf6c8c6038825
alikhan354400 <alikhan354400@gmail.com>
2024-07-09 14:57:49 +0500

    Initial commit

---
+++ javascript/java.js
@@ -0,0 +1,75 @@
+console.log("hello");
+//Show error
+console.error("error");
+
+//Show Warning
+console.warn("Warning")
+
+
+
+var name = "Ali khan";
+console.log(name)
+let fname="Smit hyd";
+console.log (fname);
+
+
+
+
+// ILLegal
+// let 1number=30;
+// console.log(1number)
+// let num*ber=40;
+// console.log(num*ber)
+
+
+
+// Legal
+var number =30;
+console.log(number)
+var number_1 =40;
+console.log(number_1)
+
+
+
+// alert
+alert("alert");
+
+
+
+// use var scope (var is re declare variable)
+// var a=20;
+// var a=30;
+
+
+
+
+// use let scope (let is not re declare variable)
+// let b=ali;
+// let b=khan;
+
+// use let scope (let is not use re declare variable) 
+// ---------------------------
+
+// let a="ali";
+// console.log(a)
+let a="shoaib";
+console.log(a)
+
+
+
+
+
+//alert Show msg box one and second line(use with \n )
+alert("Student of  \nSMIT HYD") 
+
+
+
+
+
+//use \t 
+alert("SMIT \t HYD")
+
+
+
+
+alert("Pizza\nPizz\nPiz\nPi\np")
